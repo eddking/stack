275068df2f6ab684f4d8e3d1dd11daa48de99b70
Edd King <edd@academia.edu>
2016-07-29 00:06:59 +0100

    made a space for action creators

--- src/sagas/index.ts
+++ src/sagas/index.ts
@@ -1,10 +1,12 @@
 import { takeEvery, delay } from 'redux-saga'
 import { put, call, fork } from 'redux-saga/effects'
 
+import { incrementCounter } from '../actions'
+
 function* mySaga() {
   while(true) {
     yield call(delay, 1000)
-    yield put({type: 'INCREMENT'})
+    yield put(incrementCounter())
   }
 }
 
